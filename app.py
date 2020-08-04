ffe7e1c0a0738750914a1e8f21d721f31308029a
Pablo Sao <pablo_sao@outlook.com>
2020-08-04 13:48:46 -0600

    agregando filtro de rango con fechas e indicadores promedio de hombres y mujeres

--- app.py
+++ app.py
@@ -26,23 +26,29 @@ LINK_GITHUB = ''
 PATH_DATA_FILE = 'Accidentes de Transito.xlsx'
 
 DATA_FALLECIDOS_LESIONADOS = pd.read_excel(PATH_DATA_FILE, sheet_name='Fallecidos-Lesionados')
+#print(manager.getLesionadoFallecido(DATA_FALLECIDOS_LESIONADOS,2017,2019))
 
-DATA_HECHOS_TRANSITO = pd.read_excel(PATH_DATA_FILE, sheet_name='Hechos-Transito')
+FILTRO_ANIOS = DATA_FALLECIDOS_LESIONADOS.año_ocu.unique()
+
+#DATA_HECHOS_TRANSITO = pd.read_excel(PATH_DATA_FILE, sheet_name='Hechos-Transito')
+
 
 DATA_VEHICULOS_INVOLUCRADOS = pd.read_excel(PATH_DATA_FILE, sheet_name='Vehiculos-Involucrados')
+#print(manager.getEstadoConductor(DATA_VEHICULOS_INVOLUCRADOS,2017,2019))
 
 
-print(manager.getCasosDepartamento(DATA_FALLECIDOS_LESIONADOS))
 
-#####
-q1 = """
-        SELECT 
-             tipo_eve as 'Tipo de Evento'
-            ,año_ocu as 'Año Ocurrido'
-        FROM 
-            DATA_FALLECIDOS_LESIONADOS
-      """
 
+
+#q1 = """
+#        SELECT
+#             tipo_eve as 'Tipo de Evento'
+#            ,año_ocu as 'Año Ocurrido'
+#        FROM
+#            DATA_FALLECIDOS_LESIONADOS
+#      """
+
+"""
 temp_fallecidos = psql.sqldf(q1, locals())
 
 #print( psql.sqldf(q1, locals()) )
@@ -57,28 +63,9 @@ for columnas in list(table.columns):
 table.columns = columnas_table
 table.insert(0, 'Tipo de Evento', table.index)
 
-
-#for colum in list(table.max())[1:]:
-#    print(colum)
+print(manager.getCasosMunicipio(DATA_FALLECIDOS_LESIONADOS))
 
 """
-if 'id' in table:
-    columnas_numericas = table.select_dtypes('number').drop(['id'], axis=1)
-else:
-    columnas_numericas = table.select_dtypes('number')
-
-for (columnName, columnData) in columnas_numericas.iteritems():
-    max_value = max(columnData)
-    print(max_value)
-
-print(columnas_numericas)
-"""
-
-#print(style_heatMap_table(table))
-#print(list(table.columns[1:]))
-
-#---     FIN VARIABLES ESTATICAS     ---#
-
 
 # Logo universidad del Valle de Guatemala
 file_uvg_logo = 'uvg-logo.jpg' # replace with your own image
@@ -94,8 +81,6 @@ server = app.server
 
 #Colocando titulo a la pestania
 app.title = 'Accidentes de Transito'
-
-#Layout de la pagina
 app.layout = html.Div(children=[
     dbc.Container(children=[
 
@@ -142,57 +127,213 @@ app.layout = html.Div(children=[
                                 dbc.Collapse(
                                     dbc.Card(
                                         dbc.CardBody(
-                                            "El contenido se esconde al colapsarse el panel y ahorita se anda probando si funciona el size asignado de 10 y ver como queda el espacio")
+                                            dbc.Row([
+                                                dbc.Col(children=[
+                                                    html.H6("Rango de Años:")
+                                                ],width={"size": 3,"offset": 2},align="left"),
+                                                dbc.Col(children=[
+                                                    dcc.RangeSlider(
+                                                        id = 'sl-rango-anios',
+                                                        min=min(FILTRO_ANIOS),
+                                                        max=max(FILTRO_ANIOS),
+                                                        # step=None,
+                                                        marks=manager.getLabelFiltroAnios(FILTRO_ANIOS),
+                                                        value=[min(FILTRO_ANIOS), max(FILTRO_ANIOS)]
+                                                    ),
+                                                ],width={"size": 4,},align="right"),
+                                            ],no_gutters=True,
+                                            ),
+
+
+                                            #"El contenido se esconde al colapsarse el panel y ahorita se anda probando si funciona el size asignado de 10 y ver como queda el espacio"
+
+                                        )
                                     ),
                                     id="collapse",
                                 ),
                             ]
-                        )],
-                    # width={"size": 10},
+                        )
+                    ],
                 ),
-
             ],
             justify="around",
         ),
 
         html.Br(),
 
-        dbc.Row([
-            dbc.Col(
-                children=[
-                    dash_table.DataTable(
-                        id='table',
-                        columns=[{"name": i, "id": i} for i in table.columns],
-                        data=table.to_dict('records'),
-                        style_data_conditional= manager.style_heatMap_table(table)
-                        # [
-                        #        {
-                        #            'if': {
-                        #                'filter_query': '{2017} > 1000', # comparing columns to each other
-                        #               'column_id': '2017'
-                        #            },
-                        #            'backgroundColor': '#3D9970',
-                        #            'color': 'white',
-                        #        }
-                        #    ]
-                    ),
-                ]),
+        html.Div(id='output-container-range-slider')
 
-        ]),
+        #dbc.Row([
+        #    dbc.Col(
+        #        children=[
+                    #dash_table.DataTable(
+                    #    id='table',
+                    #    columns=[{"name": i, "id": i} for i in table.columns],
+                    #    data=table.to_dict('records'),
+                    #    style_data_conditional= manager.style_heatMap_table(table)
+                    #),
+
+                    #html.Div([
+                    #    dbc.Row([
+                    #        #dbc.Col(children=[
+                    #        #    html.H3("Descripción")
+                    #        #],align="center"),
+                    #        dbc.Col(children=[
+                    #            html.H4("HOMBRES"),
+                    #            html.Div(id='output-container-range-slider')
+                    #        ],align="center"),
+                    #        dbc.Col(children=[
+                    #            html.H4("MUJERES")
+                    #        ],align="center"),
+                    #    ],align="center"
+                    #    ),
+                    #]),
+
+
+        #        ]),
+
+        #]),
 
     ],fluid=True),
 
 ])
 
+# Evento para colapsar panel
 @app.callback(
     Output("collapse", "is_open"),
     [Input("collapse-filtros", "n_clicks")],
     [State("collapse", "is_open")],
 )
-def toggle_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
+def toggle_collapse(n,is_open):
+    if is_open is None:
+        is_open = True
+        return is_open
+
+    return not is_open
+
+# Filtro de Slider
+@app.callback(
+    dash.dependencies.Output('output-container-range-slider', 'children'),
+    [dash.dependencies.Input('sl-rango-anios', 'value')])
+def update_output(value):
+
+
+    # Llamado del promedio de lesionados y fallecidos
+    avg_les_fall = manager.getLesionadoFallecido(DATA_FALLECIDOS_LESIONADOS, int(value[0]), int(value[1]))
+
+    avg_estado_conductor = manager.getEstadoConductor(DATA_VEHICULOS_INVOLUCRADOS,int(value[0]),int(value[1]))
+
+    return (
+
+        dbc.Row([
+
+            dbc.Col(children=[
+                html.H3("Masculino",id="cart-custom")
+            ],align="center"),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Fallecidos"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_les_fall['hombre']['fallecidos'],className="card-title"),
+                        ],id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ],align="center"),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Lesionados",id="cart-custom"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_les_fall['hombre']['lesionados'], className="card-title"),
+                        ],id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ]),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Bajo Efectos de Alcohol", id="cart-custom"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_estado_conductor['hombre']['Ebrios'], className="card-title"),
+                        ], id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ]),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Sobrios",id="cart-custom"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_estado_conductor['hombre']['Sobrio'], className="card-title"),
+                        ], id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ]),
+
+        ]),
+
+        html.Br(),
+
+        dbc.Row([
+
+            dbc.Col(children=[
+                html.H3("Femenino", id="cart-custom")
+            ], align="center"),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Fallecidas"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_les_fall['mujer']['fallecidos'], className="card-title"),
+                        ], id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ], align="center"),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Lesionadas", id="cart-custom"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_les_fall['mujer']['lesionados'], className="card-title"),
+                        ], id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ]),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Bajo Efectos de Alcohol", id="cart-custom"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_estado_conductor['mujer']['Ebrios'], className="card-title"),
+                        ], id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ]),
+
+            dbc.Col(children=[
+                dbc.Card(children=[
+                    dbc.CardHeader("Sobrias", id="cart-custom"),
+                    dbc.CardBody(
+                        [
+                            html.H2(avg_estado_conductor['mujer']['Sobrio'], className="card-title"),
+                        ], id="cart-custom"
+                    ),
+                ], color="info", inverse=True),
+            ]),
+
+        ]),
+
+    )
+
+    #return 'You have selected "{}"'.format(avg_les_fall['hombre']['fallecidos'])
 
 
 if __name__ == '__main__':
--- manager.py
+++ manager.py
@@ -2,50 +2,146 @@
 import math
 import pandas as pd
 import pandasql as psql
-from geopy.geocoders import Nominatim
 
-def getCoordenadas(direccion):
-    #print(direccion)
-    #address = 'Ciudad de Guatemala, Guatemala'
-    try:
-        print('Dentro del try')
-        geolocator = Nominatim(user_agent="Pablo")
-        location = geolocator.geocode(direccion)
-        return [location.latitude, location.longitude]
-    except:
-        print('dentro del except')
-        print(direccion)
-        return list()
+def getLabelFiltroAnios(anios):
+
+    LABEL_FILTRO_ANIOS = {}
+
+    for valores in anios:
+        LABEL_FILTRO_ANIOS.update({int(valores): {'label': str(valores)}})
 
 
+    return LABEL_FILTRO_ANIOS
 
-def getCasosDepartamento(DataFrame):
 
-    paises = """
+def getWhere(filtro):
+    Where = ''
+
+    for valor in range(len(filtro)):
+        if(valor == 0):
+            Where += filtro[valor]
+        Where += ' AND {0}'.format(filtro[valor])
+
+    return Where
+
+def getCasosMunicipio(dataframe):
+    q1 = """
             SELECT 
-                 depto_ocu as 'Departamento'
-                ,mupio_ocu as 'Municipio'
-                ,count(mupio_ocu) as 'Accidentes'
+                 mupio_ocu as Municipio
+                ,coordenada as Coordenada
+                ,count(coordenada) as Cantidad
             FROM 
-                DataFrame
-            GROUP BY
-                depto_ocu, mupio_ocu
+                dataframe
+            WHERE
+                coordenada <> '[]'
+            group by mupio_ocu,coordenada
           """
 
-    coordenadas = psql.sqldf(paises, locals())
+    coordenadas = psql.sqldf(q1, locals())
 
-    coordenadas['coordenadas'] = coordenadas.apply(lambda row: getCoordenadas('{0},{1}, Guatemala'.format(
-                                                                     row['Municipio'].split('-')[1]  # .replace(" ", "")
-                                                                    ,row['Departamento'].split('-')[1]  # .replace(" ", "")
-                                                                  )
-                                                                )
-                                                    , axis=1)
+    print(coordenadas)
 
-    return coordenadas
+def getLesionadoFallecido(dataframe,anio_inicial,anio_final):
 
+    resultado = {}
+
+    consulta = """
+                SELECT
+                     sexo_per
+                    ,fall_les
+                    ,round(avg(cantidad)) as cantidad
+                FROM(
+                        SELECT
+                             sexo_per
+                            ,fall_les
+                            ,año_ocu
+                            ,count(año_ocu) as cantidad
+                        FROM
+                            dataframe
+                        where
+                            año_ocu between {0} and {1} 
+                        and fall_les is not null
+                        and sexo_per <> '9 - Ignorado'
+                        group by
+                            sexo_per, fall_les,año_ocu
+                    )
+                group by sexo_per, fall_les
+                order by sexo_per
+               """.format(anio_inicial,anio_final)
+
+    datos = psql.sqldf(consulta, locals())
+    #print(datos)
+
+    resultado = {'hombre':{
+                            'fallecidos':int(datos['cantidad'].values[0]),
+                            'lesionados':int(datos['cantidad'].values[1])
+                          },
+                 'mujer':{
+                     'fallecidos': int(datos['cantidad'].values[2]),
+                     'lesionados': int(datos['cantidad'].values[3])
+                        },
+
+                 }
+
+    #print(datos['cantidad'].values[0])
+
+
+    return resultado
+
+
+def getEstadoConductor(dataframe, anio_inicial, anio_final):
+    resultado = {}
+
+    consulta = """
+                SELECT
+                     sexo_per
+                    ,estado_con
+                    ,round(avg(cantidad)) as cantidad
+                FROM(
+                        SELECT
+                             sexo_per
+                            ,estado_con
+                            ,año_ocu
+                            ,count(año_ocu) as cantidad
+                        FROM
+                            dataframe
+                        where
+                            año_ocu between {0} and {1} 
+                        and estado_con is not null
+                        and sexo_per <> '9 - Ignorado'
+                        and estado_con <> '9 - Ignorado'
+                        group by
+                            sexo_per, estado_con,año_ocu
+                    )
+                group by sexo_per, estado_con
+                order by sexo_per
+               """.format(anio_inicial, anio_final)
+
+    datos = psql.sqldf(consulta, locals())
+    #print(datos)
+
+    resultado = {'hombre': {
+        'Ebrios': int(datos['cantidad'].values[1]),
+        'Sobrio': int(datos['cantidad'].values[0])
+    },
+        'mujer': {
+            'Ebrios': int(datos['cantidad'].values[3]),
+            'Sobrio': int(datos['cantidad'].values[2])
+        },
+
+    }
+
+    #print(datos['cantidad'].values[0])
+
+    return resultado
 
 def style_heatMap_table(dataframe):
-    size_heatmap = 5
+    """
+    Devuelve el estilo para elaborar un heatmap de una tabla
+    :param dataframe: pandas DataFrame
+    :return: json con el estilo de tabla
+    """
+    size_heatmap = 10
 
     if 'id' in dataframe:
         columnas_numericas = dataframe.select_dtypes('number').drop(['id'], axis=1)
@@ -54,38 +150,55 @@ def style_heatMap_table(dataframe):
 
     styles = []
 
-    value = 0
+    value = math.floor( (max(list(columnas_numericas.max()))) / size_heatmap)
 
     for (columnName, columnData) in columnas_numericas.iteritems():
-        if(value==0):
-            value = math.floor(max(columnData) / size_heatmap)
 
         total = 0
 
         for rangos in range(size_heatmap):
 
-            color = ''
             total += value
+
+            color = ''
+            color_letra = 'white'
+            query = '{0} >= {1}'.format("{"+columnName+"}",total)
+
             if (rangos == 0):
-                color = '#d7e6f4'
+                color = '#eff5fa'
+                color_letra = 'black'
+                query = '{0} <= {1}'.format("{" + columnName + "}",total)
+
             elif (rangos == 1):
-                color = '#afcdea'
+                color = '#dbe8f5'
+                color_letra = 'black'
             elif (rangos == 2):
-                color = '#88b4e0'
+                color = '#c3daef'
+                color_letra = 'black'
             elif (rangos == 3):
-                color = '#609bd6'
+                color = '#afceea'
             elif (rangos == 4):
-                color = '#3983cc'
+                color = '#9bc2e5'
+            elif (rangos == 5):
+                color = '#87b6e0'
+            elif (rangos == 6):
+                color = '#73aadb'
+            elif (rangos == 7):
+                color = '#5f9ed6'
+            elif (rangos == 8):
+                color = '#3685cc'
+            elif (rangos == 9):
+                color = '#2b6aa3'
 
             styles.append(
 
                 {
                     'if': {
-                        'filter_query': '{0} >= {1}'.format("{"+columnName+"}", total),  # comparing columns to each other
+                        'filter_query': query,  # comparing columns to each other
                         'column_id': '{0}'.format(columnName)
                     },
                     'backgroundColor': color,
-                    'color': 'white',
+                    'color': color_letra,
                 }
 
             )
